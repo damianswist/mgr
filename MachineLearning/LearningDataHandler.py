5a09f21ec120fe00217882f88f40d4047a77cf68
Damian Swist <damian.swist@gmail.com>
2017-09-17 17:49:18 +0200

    ml ready

---
+++ MachineLearning/LearningDataHandler.py
@@ -0,0 +1,203 @@
+import pandas
+
+from DBHandler.DBHandler import DBHandler
+import csv
+import fileinput
+
+
+class LearningDataHandler(object):
+    def __init__(self, video_id, recipe):
+        self.video_id = video_id
+        self.recipe = recipe
+
+    def set_summarization(self, video_id, recipe):
+        self.video_id = video_id
+        self.recipe = recipe
+
+    def fill_a_learning_data(self):
+        recipe = self.prepare_recipe_data()
+        data = self.download_summarization_data(recipe)
+        data = self.calculate_data(data, recipe)
+        self.fill_file_with_data(data, 8)
+        return data
+
+    def fill_b_learning_data(self):
+        self.load_recipe()
+
+    def fill_c_learning_data(self):
+        self.load_recipe()
+
+    def load_recipe(self):
+        with open(self.recipe, 'r') as file:
+            content = [line.strip() for line in file]
+        return content
+
+    def prepare_recipe_data(self):
+        data = self.load_recipe()
+        data = self.convert_recipe_format(data)
+        return data
+
+    def convert_recipe_format(self, data):
+        results = list()
+        for e in data:
+            x = e.split(", ")
+            results.append({'start': int(x[0]), 'end': int(x[1])})
+        return results
+
+    def download_summarization_data(self, recipe):
+        db = DBHandler()
+        db.connect()
+        results = db.get_selected_video_data(self.video_id)
+        db.close_connection()
+        results = self.filter_frames(results, recipe)
+        return results
+
+    def filter_frames(self, data, recipe):
+        results = list()
+        for shot in recipe:
+            for x in range(shot['start'], shot['end']+1):
+                results.append(data[x])
+        return results
+
+    def calculate_data(self, data, recipe):
+        shots_data = list()
+        cnt = 0
+        for shot_period in recipe:
+            frames_number = 1 + shot_period['end'] - shot_period['start']
+            blockiness = 0.0
+            sa = 0.0
+            letterbox = 0.0
+            pillarbox = 0.0
+            blockloss = 0.0
+            blur = 0.0
+            ta = 0.0
+            blackout = 0.0
+            freezing = 0.0
+            exposure_bri = 0.0
+            contrast = 0.0
+            interlace = 0.0
+            noise = 0.0
+            slice = 0.0
+            flickering = 0.0
+            first_frame_of_shot = True
+            for x in range(cnt, cnt+frames_number):
+                if not first_frame_of_shot:
+                    # TA depends on previous frame, so if it is from another shot it should be omitted
+                    ta += float(data[x][10])
+                blockiness += float(data[x][4])
+                sa += float(data[x][5])
+                letterbox += float(data[x][6])
+                pillarbox += float(data[x][7])
+                blockloss += float(data[x][8])
+                blur += float(data[x][9])
+                blackout += float(data[x][11])
+                freezing += float(data[x][12])
+                exposure_bri += float(data[x][13])
+                contrast += float(data[x][14])
+                interlace += float(data[x][15])
+                noise += float(data[x][16])
+                slice += float(data[x][17])
+                flickering += float(data[x][18])
+
+                first_frame_of_shot = False
+                cnt += 1
+            shot = {
+                # 'frames_range': "{0}, {1}".format(shot_period['start'], shot_period['end']),
+                'blockiness': float(blockiness / frames_number),
+                'SA': float(sa / frames_number),
+                'letterbox': float(letterbox / frames_number),
+                'pillarbox': float(pillarbox / frames_number),
+                'blockloss': float(blockloss / frames_number),
+                'blur': float(blur / frames_number),
+                'TA': float(ta / (frames_number - 1)),
+                'blackout': float(blackout / frames_number),
+                'freezing': float(freezing / frames_number),
+                'exposure_bri': float(exposure_bri / frames_number),
+                'contrast': float(contrast / frames_number),
+                'interlace': float(interlace / frames_number),
+                'noise': float(noise / frames_number),
+                'slice': float(slice / frames_number),
+                'flickering': float(flickering / frames_number),
+                'grade': 0
+            }
+            shots_data.append(shot)
+        return shots_data
+
+
+    def get_number_of_last_frame_of_shots(self, recipe):
+        results = [x['end'] for x in recipe]
+        return results
+
+    def fill_file_with_data(self, data, grade):
+        rows = list()
+        for row in data:
+            x = list()
+            x.append(row['blockiness'])
+            x.append(row['SA'])
+            x.append(row['letterbox'])
+            x.append(row['pillarbox'])
+            x.append(row['blockloss'])
+            x.append(row['blur'])
+            x.append(row['TA'])
+            x.append(row['blackout'])
+            x.append(row['freezing'])
+            x.append(row['exposure_bri'])
+            x.append(row['contrast'])
+            x.append(row['interlace'])
+            x.append(row['noise'])
+            x.append(row['slice'])
+            x.append(row['flickering'])
+            x.append(grade)
+            rows.append(x)
+        with open(r"E:\video_summarization\MachineLearning\learning_data\learning_data_A.csv", 'a') as f:
+            writer = csv.writer(f, delimiter='\t')
+            writer.writerows(rows)
+
+
+    def add_columns_to_file(self, vid_cat):
+        columns = [
+            "blockiness",
+            "SA",
+            "letterbox",
+            "pillarbox",
+            "blockloss",
+            "blur",
+            "TA",
+            "blackout",
+            "freezing",
+            "exposure_bri",
+            "contrast",
+            "interlace",
+            "noise",
+            "slice",
+            "flickering",
+            "score"
+        ]
+        with open(r"E:\video_summarization\MachineLearning\learning_data\learning_data_A.csv", 'a') as f:
+            writer = csv.writer(f, delimiter='\t')
+            writer.writerow(columns)
+
+    @classmethod
+    def get_learning_data(cls, vid_cat):
+        path = r"E:\video_summarization\MachineLearning\learning_data\learning_data_A.csv"
+        dataset = pandas.read_csv(path, sep="\t")
+        # print(dataset.head(20))
+        # print(dataset.shape)
+        return dataset
+
+    def delete_empty_lines(self, file):
+        with open('path/to/file') as infile, open('output.txt', 'w') as outfile:
+            for line in infile:
+                if not line.strip(): continue  # skip the empty line
+                outfile.write(line)  # non-empty line. Write it to output
+
+if __name__ == "__main__":
+    recipe = r"E:\video_summarization\MachineLearning\YswnulN_q0w.txt"
+    vid_id = "YswnulN_q0w"
+    handler = LearningDataHandler(vid_id, recipe)
+    # handler.add_columns_to_file("a")
+    data = handler.fill_a_learning_data()
+    # for d in data:
+    #     print(d)
+    # handler.add_columns_to_file("a")
+    # handler.get_learning_data("a")
---
+++ MachineLearning/MachineLearningHandler.py
@@ -0,0 +1,106 @@
+from MachineLearning.LearningDataHandler import LearningDataHandler
+
+import pandas
+from pandas.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+from sklearn import model_selection
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+
+class MachineLearningHandler(object):
+    def load_learning_data(self, video_category):
+        data_set = LearningDataHandler.get_learning_data("a")
+        return data_set
+
+    def print_dimension(self, data_set):
+        print(data_set.shape)
+
+    def show_data(self, data_set):
+        print(data_set.head(20))
+
+    def print_learning_data_stats(self, data_set):
+        print(data_set.describe())
+
+    def print_grouped_by_score(self, data_set):
+        print(data_set.groupby('score').size())
+
+    def visualize_data(self, dataset):
+        dataset.plot(kind='box', subplots=True, layout=(4, 4), sharex=False, sharey=False)
+        plt.show()
+
+    def display_histograms(self, data_set):
+        data_set.hist()
+        plt.show()
+
+    def display_interactions_between_variables(self, data_set):
+        scatter_matrix(data_set)
+        plt.show()
+
+    def do_magic(self, data_set):
+        array = data_set.values
+        X = array[:, 0:15]
+        Y = array[:, 15]
+        validation_size = 0.20
+        seed = 7
+        X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size,
+                                                                                        random_state=seed)
+
+        # Test options and evaluation metric
+        seed = 7
+        scoring = 'accuracy'
+
+        # Spot Check Algorithms
+        models = []
+        models.append(('LR', LogisticRegression()))
+        # models.append(('LDA', LinearDiscriminantAnalysis()))
+        models.append(('KNN', KNeighborsClassifier()))
+        models.append(('CART', DecisionTreeClassifier()))
+        models.append(('NB', GaussianNB()))
+        models.append(('SVM', SVC()))
+        # evaluate each model in turn
+        results = []
+        names = []
+        for name, model in models:
+            kfold = model_selection.KFold(n_splits=10, random_state=seed)
+            cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+            results.append(cv_results)
+            names.append(name)
+            msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+            print(msg)
+
+        # fig = plt.figure()
+        # fig.suptitle('Algorithm Comparison')
+        # ax = fig.add_subplot(111)
+        # plt.boxplot(results)
+        # ax.set_xticklabels(names)
+        # plt.show()
+
+        knn = KNeighborsClassifier()
+        knn.fit(X_train, Y_train)
+        predictions = knn.predict(X_validation)
+        print("Accuracy score")
+        print(accuracy_score(Y_validation, predictions))
+        print("Confusion matrix")
+        print(confusion_matrix(Y_validation, predictions))
+        print("Classification result")
+        print(classification_report(Y_validation, predictions))
+
+if __name__ == "__main__":
+    handler = MachineLearningHandler()
+    data_set = handler.load_learning_data("a")
+    # handler.print_dimension(data_set)
+    # handler.show_data(data_set)
+    # handler.print_learning_data_stats(data_set)
+    # handler.print_grouped_by_score(data_set)
+    # handler.visualize_data(data_set)
+    # handler.display_histograms(data_set)
+    # handler.display_interactions_between_variables(data_set)  # ?????????????
+    handler.do_magic(data_set)
