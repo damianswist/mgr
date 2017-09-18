9fe109d940400960425c7b68a90a66789ff639a9
Damian Swist <damian.swist@gmail.com>
2017-09-18 22:42:47 +0200

    after refactor

--- MachineLearning/tutorial2.py
+++
@@ -1,9 +0,0 @@
-from matplotlib.mlab import PCA
-from sklearn import datasets
-import matplotlib.pyplot as plt
-# Load in the `digits` data
-from sklearn.decomposition import RandomizedPCA
-
-digits = datasets.load_digits()
-
-
